fce8d3f4187b188ffc330ae1e855042b613ab44f
Arnon Nascimento <arnon.com.n@gmail.com>
2022-06-29 12:15:44 -0300

    Primeiro commit do projeto

---
+++ Project Amazon SQS/Project Amazon SQS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Amazon_SQS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
