a175a8c571fbb46e4930794fc6fd28a23b532e6f
Arnon Nascimento <arnon.com.n@gmail.com>
2022-06-30 16:18:14 -0300

    ultimo commit

--- Project Amazon SQS/SQS.Envia/Program.cs
+++ Project Amazon SQS/SQS.Envia/Program.cs
@@ -3,23 +3,41 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using Amazon.Runtime.CredentialManagement;
+
+
 
 namespace SQS.Envia
 {
+  
     class Program
     {
-        static async Task Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Enviando mensagem para a fila...");
-
-            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
-            var request = new SendMessageRequest
+            try
             {
-                QueueUrl = "[Pegar a Queue URL com Itallo]",
-                MessageBody = "Itallo BoBaião :P"
-            };
+                var options = new CredentialProfileOptions
+                {
+                    AccessKey = "[sua AccessKey aqui]",
+                    SecretKey = "[sua SecretKey aqui]"
+                };
 
-            await client.SendMessageAsync(request);
+                var client = new AmazonSQSClient(options.AccessKey, options.SecretKey, RegionEndpoint.SAEast1);
+                var request = new SendMessageRequest
+                {
+                    QueueUrl = "[SUA QUEUEURL AQuI]",
+                    MessageBody = "Sua mensagem aqui",
+                    MessageGroupId = new Random(1000).ToString(),
+                    MessageDeduplicationId = new Random(1000).ToString()
+                };
+
+                 client.SendMessageAsync(request).Wait();
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
     }
 }
--- Project Amazon SQS/SQS.Recebe/Program.cs
+++ Project Amazon SQS/SQS.Recebe/Program.cs
@@ -3,30 +3,43 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using Amazon.Runtime.CredentialManagement;
 
 namespace SQS.Recebe
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Oia as mensagem ai meu nobre ...");
-
-            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
-            var request = new ReceiveMessageRequest
+            try
             {
-                QueueUrl = "[Pegar a Queue URL com Itallo]"
-            };
-
-            while (true)
-            {
-                var response = await client.ReceiveMessageAsync(request);
-
-                foreach (var mensagem in response.Messages)
+                var options = new CredentialProfileOptions
                 {
-                    Console.WriteLine(mensagem.Body);
-                    await client.DeleteMessageAsync("[Pegar a Queue URL com Itallo]", mensagem.ReceiptHandle);
+                    AccessKey = "sua AccessKey aqui",
+                    SecretKey = "sua SecretKey aqui"
+                };
+                Console.WriteLine("Oia as mensagem ai meu nobre ...");
+
+                var client = new AmazonSQSClient(options.AccessKey, options.SecretKey, RegionEndpoint.SAEast1);
+                var request = new ReceiveMessageRequest
+                {
+                    QueueUrl = "[SUA QUEUEURL AQuI]"
+                };
+
+                while (true)
+                {
+                    var response = await client.ReceiveMessageAsync(request);
+
+                    foreach (var mensagem in response.Messages)
+                    {
+                        Console.WriteLine(mensagem.Body);
+                        await client.DeleteMessageAsync("[SUA QUEUEURL AQuI]", mensagem.ReceiptHandle);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+            
             }
         }
     }
